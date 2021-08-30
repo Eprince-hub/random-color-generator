b2e9fcaf8fd1ba0d5d0263cccbb2cdd3e15d0397
VICTOR EJIKE NWOSU <74430629+Eprince-hub@users.noreply.github.com>
2021-08-30 22:28:01 +0200

    added codes to the files

---
+++ app.js
@@ -0,0 +1,54 @@
+let colorBox = document.querySelector('.colorBox');
+let mainBody = document.querySelector('#main_section');
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+let button = document.querySelector('#button');
+
+para1.innerHTML = 'The Hex Value: #000000 ';
+para2.innerHTML = 'The RGB Value: rgb(00, 00, 00)';
+
+mainBody.appendChild(para1);
+mainBody.appendChild(para2);
+
+function callColors() {
+	function randomNumber() {
+		const dings = ['A', 'B', 'C', 'D', 'E', 'F'];
+		let finalResult = '#';
+
+		for (let i = 0; i < 3; i++) {
+			let number = Math.floor(Math.random() * 10);
+			let alphabet = Math.floor(Math.random() * dings.length);
+			let result = dings[alphabet];
+			result += number;
+			finalResult += result;
+		}
+		return finalResult;
+	}
+	//randomNumber();
+	let rgbColor = randomNumber();
+	let newValue = rgbColor.substring(1);
+
+	function convertToRGB(hexValue) {
+		let regex = /.{1,2}/g;
+		let newHexValue = hexValue.match(regex);
+		let aRgb = [
+			parseInt(newHexValue[0], 16),
+			parseInt(newHexValue[1], 16),
+			parseInt(newHexValue[2], 16),
+		];
+		return aRgb;
+	}
+
+	//convertToRGB(newValue);
+	let rgbValue = convertToRGB(newValue);
+
+	para1.innerHTML = 'The Hex Value: ' + rgbColor;
+	para2.innerHTML = `The RGB Value: rgb(${rgbValue})`;
+
+	mainBody.appendChild(para1);
+	mainBody.appendChild(para2);
+
+	colorBox.style.backgroundColor = `${rgbColor}`;
+}
+
+button.addEventListener('click', callColors);
